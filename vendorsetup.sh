61286ee3644841c4693e629fe918ec8bd7d8e725
The Sapphire <114116628+blksapphire@users.noreply.github.com>
2025-07-06 23:13:49 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_Infinix-X672-user
-add_lunch_combo omni_Infinix-X672-userdebug
-add_lunch_combo omni_Infinix-X672-eng
+add_lunch_combo twrp_X672-user
+add_lunch_combo twrp_X672-userdebug
+add_lunch_combo twrp_X672-eng
