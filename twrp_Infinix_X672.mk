730dc8dbc479cb84e39c8fd6736d388d8e4d4d39
The Sapphire <114116628+blksapphire@users.noreply.github.com>
2025-07-06 20:29:19 +0100

    Correct Device Code name before errors

--- twrp_Infinix_X672.mk
+++ twrp_Infinix_X672.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from Infinix-X672 device
-$(call inherit-product, device/infinix/Infinix-X672/device.mk)
+$(call inherit-product, device/infinix/X672/device.mk)
 
-PRODUCT_DEVICE := Infinix-X672
-PRODUCT_NAME := twrp_Infinix_X672
+PRODUCT_DEVICE := X672
+PRODUCT_NAME := twrp_X672
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X672
 PRODUCT_MANUFACTURER := infinix
